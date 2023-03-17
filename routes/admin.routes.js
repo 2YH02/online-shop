52f17280020b548d91ac03d6dd7de36a965a973e
young <yonghuni484@gamil.com>
2023-03-17 10:35:20 +0900

    Feat product upload

---
+++ routes/admin.routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const adminController = require("../controllers/admin.controller");
+const imageUploadMiddleware = require("../middlewares/image-upload");
+
+const router = express.Router();
+
+router.get("/products", adminController.getProducts);
+router.post(
+  "/products",
+  imageUploadMiddleware,
+  adminController.createNewProducts
+);
+
+router.get("/products/new", adminController.getNewProducts);
+
+module.exports = router;
